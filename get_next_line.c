e5313b962235851d099ad23c41174925adedfe7f
Jean-baptiste TERRAZZONI <jterrazz@e1r2p12.42.fr>
2017-05-07 21:42:22 +0200

    soon

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,25 @@
 /*   By: jterrazz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/07 21:32:14 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/05/07 21:32:16 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/05/07 21:42:14 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "GET_NEXT_LINE_H"
+#include <fcntl.h>
 
+// si static buff alors read du buff sinon read
+
+int		get_next_line(const int fd, char **line)
+{
+	
+}
+
+// DELETE AT END !!!
+int 	main(int argc, char *argv)
+{
+	char *line = "lolilol";
+	int fd = open(argv[1], O_RDONLY);
+	get_next_line(fd, &line);
+	return (0);
+}
